f5ade41375f24d384d3ffa8713afffdee24cddaf
Weavejer26 <103794564+weavejer26@users.noreply.github.com>
2023-01-17 18:23:57 -0700

    Variable Assignment 1/17/2023

---
+++ Variable_Assignment.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace Variables_Assignment_2023
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name = "John";
+      name = "Do you know the Muffin Man?"; //I thought it would be fun to just do random stuff cause why not!
+      int age = 42;
+      age = 22;
+      int MATH = 357*4865;
+      double Deci = 15.66;
+      bool GhostBscry = true;
+      float weeeeee = 37.24f;
+      string yes = "The Muffin Man?";
+      char Letter = 'Q';
+      long LONGG = 44444;
+      string GASP = "THE MUFFIN MAN!";
+      
+      
+      
+      
+      Console.WriteLine(name); 
+      Console.WriteLine(age); 
+      Console.WriteLine(MATH); 
+      Console.WriteLine(Deci); 
+      Console.WriteLine(GhostBscry); 
+      Console.WriteLine(weeeeee); 
+      Console.WriteLine(yes); 
+      Console.WriteLine(Letter); 
+      Console.WriteLine(LONGG); 
+      Console.WriteLine(GASP); 
+       
+   }
+  }
+}
