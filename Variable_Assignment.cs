50d6b0324f7de1739c07de60cb30278a77437911
weavejer26 <weavejer26@gmail.com>
2023-01-17 18:58:06 -0700

    Added more Variables

--- Variable_Assignment.cs
+++ Variable_Assignment.cs
@@ -10,12 +10,15 @@ namespace Variables_Assignment_2023
       name = "Do you know the Muffin Man?"; //I thought it would be fun to just do random stuff cause why not!
       int age = 42;
       age = 22;
+      int bwah =37;
       int MATH = 357*4865;
       double Deci = 15.66;
       bool GhostBscry = true;
+      bool OOO = false;
       float weeeeee = 37.24f;
       string yes = "The Muffin Man?";
       char Letter = 'Q';
+      char Other = 'W';
       long LONGG = 44444;
       string GASP = "THE MUFFIN MAN!";
       
@@ -24,12 +27,15 @@ namespace Variables_Assignment_2023
       
       Console.WriteLine(name); 
       Console.WriteLine(age); 
+      Console.WriteLine(bwah); 
       Console.WriteLine(MATH); 
       Console.WriteLine(Deci); 
       Console.WriteLine(GhostBscry); 
+      Console.WriteLine(OOO);
       Console.WriteLine(weeeeee); 
       Console.WriteLine(yes); 
       Console.WriteLine(Letter); 
+      Console.WriteLine(Other);
       Console.WriteLine(LONGG); 
       Console.WriteLine(GASP); 
        
