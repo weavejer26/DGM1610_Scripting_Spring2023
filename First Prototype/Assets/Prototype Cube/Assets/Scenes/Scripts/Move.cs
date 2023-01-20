f5e585bb6432ec49fb308bdbdd9925238228be73
weavejer26 <weavejer26@gmail.com>
2023-01-19 17:46:40 -0700

    Cube Game Proto

---
+++ First Prototype/Assets/Prototype Cube/Assets/Scenes/Scripts/Move.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private float hInput;
+
+    private float vInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       hInput = Input.GetAxis("Horizontal"); 
+       vInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * hInput);
+        transform.Translate(Vector3.forward * vInput);
+    
+    }
+}
---
+++ First Prototype/Assets/Prototype Cube/Assets/Scenes/Scripts/Variables.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    // [Access Modifier] [Data-Type/ Type] [Identifier/Name] [Access Operator =] [Value]
+
+    public string herosName = "Batman";
+    
+    public int herosAge = 30;
+
+    public float heroHeight = 6.2f;
+
+    public char heroLetter = 'B';
+    
+    public double heroNetworth = 100.45;
+
+    public bool isHeroInv = false;
+    
+    
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
