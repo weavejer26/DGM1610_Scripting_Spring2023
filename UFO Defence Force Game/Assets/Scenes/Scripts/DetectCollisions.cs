737712b2f26e604b409518ef85917d6bb08e8a9f
weavejer26 <weavejer26@gmail.com>
2023-02-17 14:49:44 -0700

    Enemy Ships

---
+++ UFO Defence Force Game/Assets/Scenes/Scripts/DetectCollisions.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    //private ScoreManager scoreManager; //A variable to hold the reference to the scoremanager
+    //public int scoreToGive;
+    //public ParticleSystem explosionParticle; // Store the particle system
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       //scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>(); //Reference scoremanager
+    }
+
+   
+    void OnTriggerEnter(Collider other) 
+    {
+       if(other.gameObject.CompareTag("Blaster"))
+       {
+            Destroy(gameObject); //Destroy this game Object (UFO)
+            Destroy(other.gameObject); //Destroy the other game object it hits
+       }
+       
+        //Explosion():
+        //scoreManager.IncreaseScore(scoreToGive); //Increase Score
+    }
+
+    /*
+    void Explosion()
+    {
+        Instantiate(explosionParticle, transform.position, transform.rotation);
+    }
+    */
+
+
+
+
+
+
+
+
+
+
+
+}
